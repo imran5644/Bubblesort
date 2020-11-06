416b67ac2eb64378654cc6550e4d7881e2fbac2f
imran5644 <imi56444@gmail.com>
2020-11-07 02:30:04 +0530

    removed the errors

--- main.rb
+++ main.rb
@@ -1,36 +1,33 @@
 # frozen_string_literal: true
+
 def bubble_sort(arr)
-    j = 0
-    while j < arr.length do
-        i = 0
-        while  i < arr.length-1 do
-           if (arr[i] > arr[i+1])
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-           end
-           i+=1
-        end
-        j+=1
+  j = 0
+  while j < arr.length
+    i = 0
+    while i < arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+      i += 1
     end
-    p arr
+    j += 1
+  end
+  p arr
 end
 
 bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(arr)
-    j = 0
-    while j < arr.length do
-        i = 0
-        while  i < arr.length-1 do
-            if yield(arr[i], arr[i+ 1]).positive?
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-           end
-           i+=1
-        end
-        j+=1
+  j = 0
+  while j < arr.length
+    i = 0
+    while i < arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
+      i += 1
     end
-   p arr
+    j += 1
+  end
+  p arr
 end
 
 bubble_sort_by(%w[hi hello hey]) do |left, right|
-    left.length - right.length
-  end
+  left.length - right.length
+end
