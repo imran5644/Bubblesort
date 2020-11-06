b59d239a7fc3e18d8625d5eb5901d07ffd807a6d
imran5644 <imi56444@gmail.com>
2020-11-07 00:10:42 +0530

    implement bubble_sort_by method

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-def bubblesort(arr)
+def bubble_sort(arr)
     j = 0
     while j < arr.length do
         i = 0
@@ -14,3 +14,18 @@ def bubblesort(arr)
 end
 
 bubblesort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(arr)
+    j = 0
+    while j < arr.length do
+        i = 0
+        while  i < arr.length do
+            if yield(arr[i], arr[i+ 1]).positive?
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+           end
+           i+=1
+        end
+        j+=1
+    end
+    arr
+end
